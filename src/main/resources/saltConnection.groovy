a61e4751ea09ecd27e121d3842aafb44c35b9512
grant@iowntheinter.net <grant@iowntheinter.net>
2016-02-06 20:27:52 -0700

    cleanup

--- src/main/resources/saltConnection.groovy
+++ src/main/resources/saltConnection.groovy
@@ -19,6 +19,7 @@ URI uri = URI.create("http://127.0.0.1:8000");
 SaltStackClient client = new SaltStackClient(uri);
 cfg = client.getConfig()
 cfg.put(ClientConfig.SOCKET_TIMEOUT , 0)
+//auth as a unix user (PAM) on the salt-master system
 def UN = "user"
 def pass = "changeme"
 def token;
@@ -36,51 +37,3 @@ EventListener sr = new saltReactor(vertx,new JsonObject(), client)
 
 es = new EventStream(cfg)
 es.addEventListener(sr)
-
-//   t = new MinionList("balthazarServer")
-//   cmd="zfs.list"
-
-
-//   ri = client.callSync( new LocalCall(cmd, Optional.empty() ,Optional.empty(), returnMap[cmd] ),t)
-//
-//  println("sync call returned : ${ri}")
-
-
-
-
-/* **************************async************************************
-this dosnet work?
-
-     ar = client.callAsync( new LocalCall(cmd, Optional.empty() ,Optional.empty(), returnMap[cmd] ),t)
-     String jid = ar.getJid().toString()
-     println("jobid: ${jid}")
-     ris = client.getJobResult(jid) //something freeks out here, and this blocks hard for some reason... harder then the  sync methods
-
-     caught exception: com.google.gson.JsonSyntaxException: java.lang.IllegalStateException: Expected a string but was BEGIN_ARRAY at line 1 column 78 path $.info[0].Target
-
-     ris.each { ri ->
-         ri.getResults().each { minion, ans ->
-             println "result: ${minion} : ${ans}"
-         }
-     }
-     ******************async****************************
-     */
-
-
-//println("jid: ${ar.getJid()} mins: ${ar.getMinions()}")
-//def jid = job.getJid()
-//println("job id: " + jid);
-
-//ResultInfoSet runjob = client.getJobResult(job)
-
-//println("this job: " )
-//runjob.each {it -> println(it.getResults())}
-//client.events()
-/*
-jobs.each({k,v ->
-    println(k)
-    println(jobs.get(k))
-})*/
-/*} catch(e){
-  println("caught exception: ${e.toString()} \n"+e.getStackTrace())
-}*/
